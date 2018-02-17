3dece6c08030511677617332628e6a46ec73ecc7
Kamil Kowalski <kowalski.tychy@gmail.com>
2018-02-17 18:24:17 +0100

    Add simple search with fake data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,10 @@
 class SearchController < ApplicationController
   def index
+    @query = params[:q]
+    @results = if @query.present?
+      Search::Result.random
+    else
+      []
+    end
   end
 end
