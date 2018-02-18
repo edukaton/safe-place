5c88c87f5de251c77d03d45aacc53aa404519896
Kamil Kowalski <kowalski.tychy@gmail.com>
2018-02-18 16:00:48 +0100

    Loosen up search constraints

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     @query = params[:q].to_s
     query_tokens = @query.split(" ").map(&:downcase)
-    query_matches = ["kino", "laguna"] - query_tokens == []
+    query_matches = query_tokens.any? { |token| token.in?(%w[kino laguna]) }
     @results = if @query.present? && query_matches
       Search::Result.random
     else
